541c4bf384e7d7044c66e518d0ac1b6d215bf231
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-05-31 14:40:27 +0530

    Update 20230511172058-add-sportId-in-cancel-session.js

--- migrations/20230511172058-add-sportId-in-cancel-session.js
+++ migrations/20230511172058-add-sportId-in-cancel-session.js
@@ -2,16 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('CancelSessions', 'sportSessionId', {
-      type: Sequelize.UUID,
-      allowNull: false,
-      references: {
-        model: 'SportSessions',
-        key: 'id'
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE'
-    });
+    const table = await queryInterface.describeTable('CancelSessions');
+    if (!table.hasOwnProperty('sportSessionId')) {
+      await queryInterface.addColumn('CancelSessions', 'sportSessionId', {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'SportSessions',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
