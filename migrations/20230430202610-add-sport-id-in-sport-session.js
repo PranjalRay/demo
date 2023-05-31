02b6fef24b0369d72b2ca2ec2c2d9601a89913d1
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-05-31 14:29:12 +0530

    Update 20230430202610-add-sport-id-in-sport-session.js

--- migrations/20230430202610-add-sport-id-in-sport-session.js
+++ migrations/20230430202610-add-sport-id-in-sport-session.js
@@ -2,6 +2,14 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Check if the column already exists
+    const tableInfo = await queryInterface.describeTable('SportSessions');
+    if (tableInfo && tableInfo.sportId) {
+      console.log('Column already exists in SportSessions table');
+      return;
+    }
+
+    // Add the column
     await queryInterface.addColumn('SportSessions', 'sportId', {
       type: Sequelize.UUID,
       allowNull: false,
