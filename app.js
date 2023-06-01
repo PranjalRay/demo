b19602f70227f483f7c1055bd522e1a7b9a91ab7
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-01 15:32:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,195 +1,138 @@
-
-
-// Import necessary modules and dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-
-// Import models
-const CancelSession = require('./models/CancelSession');
-const Index = require('./models/Index');
-const Sport = require('./models/Sport');
-const SportSession = require('./models/SportSession');
-const Todo = require('./models/Todo');
-const User = require('./models/User');
-
-// Create an instance of Express
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const { request, response } = require("express");
+const express = require("express");
+const csrf = require("tiny-csrf");
 const app = express();
-
-// Set up middleware
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Middleware to set 'message' variable
-app.use((req, res, next) => {
-  res.locals.message = ''; // Initialize with an empty message
+const { Sport, User, Sessions, playerSessions } = require("./models");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+const flash = require("connect-flash");
+app.set("views", path.join(__dirname, "views"));
+app.use(flash());
+const bcrypt = require("bcrypt");
+const sport = require("./models/sport");
+const saltRounds = 10;
+app.use(
+  session({
+    secret: "my-super-secret-key-21728172615261563",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
   next();
 });
-
-// Routes
-app.get('/', (req, res) => {
-  res.render('index');
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ where: { email: username } })
+        .then(async function (user) {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch((error) => {
+          return done(null, false, {
+            message: "Your account doesn't exist, try signing up",
+          });
+        });
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
 });
-
-// Admin routes
-app.get('/admin/createSport', (req, res) => {
-  res.render('admin/createSport'); // Render the createSport.ejs file in the admin folder
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
 });
-
-app.post('/admin/sports', (req, res) => {
-  // Create a new sport
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).send('Sport name is required');
-  }
-  const sport = { id: uuidv4(), name };
-  sports.push(sport);
-  res.send(`Sport ${name} created successfully`);
+// GET route for home page
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home",
+    csrfToken: req.csrfToken(),
+  });
 });
-
-// User routes
-app.get('/signup', (req, res) => {
-  // Display the signup form
-  res.render('signup', { message: res.locals.message });
-});
-
-app.post('/signup', async (req, res) => {
-  // Handle user signup
+// POST route for user signup
+app.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    res.locals.message = 'Name, email, and password are required';
-    return res.redirect('/signup');
-  }
-  const existingUser = users.find(u => u.email === email);
-  if (existingUser) {
-    res.locals.message = 'User with that email already exists';
-    return res.redirect('/signup');
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = { id: uuidv4(), name, email, password: hashedPassword };
-  users.push(user);
-  res.send('Signup successful');
-});
-
-app.get('/login', (req, res) => {
-  // Display the signin form
-  res.render('login', { message: res.locals.message });
-});
-app.post('/login', async (req, res) => {
-  // Handle user signin
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).send('Email and password are required');
-  }
-  const user = users.find(u => u.email === email);
-  if (!user) {
-    return res.status(400).send('User not found');
-  }
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) {
-    return res.status(400).send('Invalid credentials');
-  }
-  req.session.userId = user.id;
-  res.send('Signin successful');
-});
-
-// Admin routes
-
-app.get('/admin/admin-signin', (req, res) => {
-  // Display the admin sign-in form
-  res.render('admin/admin-signin');
-});
-
-app.post('/admin/admin-signin', (req, res) => {
-  // Handle admin sign-in
-  const { email, password } = req.body;
-  // Check if the email and password match the admin credentials
-  if (email === 'admin@admin.com' && password === 'admin') {
-    // Admin sign-in successful
-    // Set a flag in the session to indicate that the user is an admin
-    req.session.isAdmin = true;
-    res.redirect('/admin/index'); // Redirect to the index.ejs file in the admin folder
-  } else {
-    // Invalid admin credentials
-    res.status(400).send('Invalid admin credentials');
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = await User.create({ name, email, password: hashedPassword });
+    req.flash("success", "Signup successful");
+    res.redirect("/login");
+  } catch (error) {
+    req.flash("error", "Signup failed");
+    res.redirect("/signup");
   }
 });
+// GET route for user login
+app.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login",
+    csrfToken: req.csrfToken(),
+  });
+});
 
-app.get('/admin/index', (req, res) => {
+// POST route for user login
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/sessions",
+  failureRedirect: "/login",
+  failureFlash: true,
+}));
+
+// GET route for user dashboard (protected route)
+app.get("/dashboard", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("dashboard", {
+    title: "Dashboard",
+    user: req.user,
+  });
+});
+
+// GET route for user logout
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// GET route for admin panel (protected route)
+app.get("/admin", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
   // Check if the user is an admin
-  if (req.session.isAdmin) {
-    res.render('admin/index'); // Render the index.ejs file in the admin folder
+  if (req.user.isAdmin) {
+    res.render("admin", {
+      title: "Admin Panel",
+      user: req.user,
+    });
   } else {
-    res.redirect('/admin/admin-signin'); // Redirect to the admin sign-in page if the user is not an admin
+    res.redirect("/dashboard");
   }
 });
-
-app.get('/signout', (req, res) => {
-  // Handle user signout
-  req.session.destroy();
-  res.send('Signout successful');
-});
-
-app.get('/sessions/create', (req, res) => {
-  // Display the form to create a new session
-  res.render('create-session', { message: res.locals.message });
-});
-
-app.post('/sessions/create', (req, res) => {
-  // Create a new session
-  const { sportId, teamA, teamB, additionalPlayers, dateTime, venue } = req.body;
-  if (!sportId || !teamA || !teamB || !additionalPlayers || !dateTime || !venue) {
-    res.locals.message = 'All fields are required';
-    return res.redirect('/sessions/create');
-  }
-  const session = {
-    id: uuidv4(),
-    sportId,
-    teamA,
-    teamB,
-    additionalPlayers,
-    dateTime,
-    venue,
-    createdBy: req.session.userId
-  };
-  sessions.push(session);
-  res.send('Session created successfully');
-});
-
-app.get('/sessions', (req, res) => {
-  // Display a list of sessions
-  res.render('sessions');
-});
-
-app.get('/sessions/:id', (req, res) => {
-  // Display details of a specific session
-  const sessionId = req.params.id;
-  const session = sessions.find(s => s.id === sessionId);
-  if (!session) {
-    return res.status(404).send('Session not found');
-  }
-  res.send(`Details of session ${sessionId}`);
-});
-
-app.post('/sessions/:id/join', (req, res) => {
-  // Join a session
-  const sessionId = req.params.id;
-  const session = sessions.find(s => s.id === sessionId);
-  if (!session) {
-    return res.status(404).send('Session not found');
-  }
-  // Add the user to the session
-  res.send(`Joined session ${sessionId}`);
-});
-
-module.exports = app;
