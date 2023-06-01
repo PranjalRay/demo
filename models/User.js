fd7d2a07131dd09865bec46dd3e809ff0ed4b3f5
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-01 16:18:09 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,20 @@
-class User {
-  constructor(id, name, email, password) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
-}
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database'); // Import the Sequelize instance
+
+const User = sequelize.define('User', {
+  // Define the attributes of the User model
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = User;
